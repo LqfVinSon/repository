bf8e596d8b59ff3f22a148ce9b45a419e5e41a0e
VinSon <972738940@qq.com>
2020-03-23 01:07:47 +0800

    把本地工程添加到git

---
+++ githubtest/src/com/ithiema/git/gitclass.java
@@ -0,0 +1,13 @@
+package com.ithiema.git;
+
+/**
+ * @auther VinSon
+ * @date 2020/3/23 0:47
+ */
+public class gitclass {
+    public static void main(String[] args) {
+        System.out.println( "hello");
+        System.out.println( "hello");
+        System.out.println( "hello");
+    }
+}
